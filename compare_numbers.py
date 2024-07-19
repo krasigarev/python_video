fbeceb32dd17a420dd41f154201b7f2b370947f3
krasigarev <kgarev@gmail.com>
2024-07-19 10:19:43 +0300

    first commit

---
+++ compare_numbers.py
@@ -0,0 +1,8 @@
+number_1 = int(input())
+number_2 = int(input())
+number_3 = int(input())
+
+if number_1 == number_2 and number_2 == number_3:
+    print("Yes")
+else:
+    print("No")
--- number_100_200.py
+++ number_100_200.py
@@ -6,3 +6,12 @@ elif number >= 100 and number <= 200:
     print("Between 100 and 200")
 else:
     print("Greater than 200")
+
+number = int(input())
+
+if number < 100:
+    print("Less than 100")
+if number >= 100 and number <= 200:
+    print("Between 100 and 200")
+else:
+    print("Greater than 200")
