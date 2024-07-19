ec3754247bd6aeceaeae204e6f09a69386ac6dbb
krasigarev <kgarev@gmail.com>
2024-07-19 11:00:26 +0300

    first commit

---
+++ lica_na_figuri.py
@@ -0,0 +1,22 @@
+import math
+
+figure = input().lower()
+
+if figure == "square":
+    side = float(input())
+    area = side * side
+    print(f"{area:.3f}")
+elif figure == "rectangle":
+    side_1 = float(input())
+    side_2 = float(input())
+    area = side_1 * side_2
+    print(f"{area:.3f}")
+elif figure == "circle":
+    r = float(input())
+    area = math.pi * math.pow(r, 2)
+    print(f"{area:.3f}")
+elif figure == "triangle":
+    a = float(input())
+    h = float(input())
+    area = a * h /2
+    print(f'{area:.3f}')
