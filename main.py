52e934167fdae736f257335f8110df8955be1edd
krasigarev <kgarev@gmail.com>
2024-07-19 08:55:58 +0300

    first commit

--- main.py
+++ main.py
@@ -2,3 +2,11 @@ grade = float(input())
 
 if grade >= 5.50:
     print("Excelent!")
+
+number_1 = int(input())
+number_2 = int(input())
+
+if number_1 > number_2:
+    print(number_1)
+else:
+    print(number_2)
